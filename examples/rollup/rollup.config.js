e94ebfe93669af8bea74a324eb3ac0d8a4424d79
foisonocean <hronro@gmail.com>
2018-07-28 11:30:51 +0800

    Add rollup example

---
+++ examples/rollup/rollup.config.js
@@ -0,0 +1,76 @@
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+import typescript from 'rollup-plugin-typescript2';
+import transformInferno from 'ts-transform-inferno';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+import { uglify } from 'rollup-plugin-uglify';
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const tsTransformer = () => ({
+  before: [
+    transformInferno()
+  ],
+  after: []
+})
+
+const config = {
+  input: 'src/index.tsx',
+  plugins: [
+    nodeResolve({
+      jsnext: true,
+      main: true,
+      customResolveOptions: {
+        packageFilter(pkg) {
+          if (!isProd && pkg['dev:module'] != null) {
+            pkg.main = pkg['dev:module'];
+          } else if (pkg.module != null) {
+            pkg.main = pkg.module;
+          } else if (pkg['js:next'] != null) {
+            pkg.main = pkg['js:next'];
+          }
+          return pkg;
+        },
+      },
+    }),
+    commonjs({
+      include: 'node_modules/**',
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
+    }),
+    typescript({
+      transformers: [tsTransformer]
+    }),
+  ],
+  output: {
+    file: 'dist/app.js',
+    format: 'iife',
+  },
+};
+
+if (!isProd) {
+  config.plugins.push(
+    serve({
+      verbose: false,
+      contentBase: '.',
+      historyApiFallback: true,
+      host: '0.0.0.0',
+      port: 3000,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+    }),
+    livereload({
+      watch: 'dist',
+    }),
+  );
+} else {
+  config.plugins.push(
+    uglify(),
+  );
+}
+
+export default config;
---
+++ examples/rollup/src/index.tsx
@@ -0,0 +1,25 @@
+import { render, version, Component } from "inferno";
+import { Incrementer } from "./components/Incrementer";
+
+const container = document.getElementById("app");
+
+class MyComponent extends Component<any, any> {
+  private tsxVersion: number;
+
+  constructor(props, context) {
+    super(props, context);
+
+    this.tsxVersion = 2.71; /* This is typed value */
+  }
+
+  public render() {
+    return (
+      <div>
+        <h1>{`Welcome to Inferno ${version} TSX ${this.tsxVersion}`}</h1>
+        <Incrementer name={"Crazy button"} />
+      </div>
+    );
+  }
+}
+
+render(<MyComponent />, container);
