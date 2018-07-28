2287d4cbe5bd46affd06cae0da8e71a16edbd4d6
foisonocean <hronro@gmail.com>
2018-07-28 11:30:51 +0800

    Add rollup example

---
+++ examples/rollup/src/components/Visualizer.tsx
@@ -0,0 +1,11 @@
+/*
+ * This is example of Inferno functional component
+ * Functional components provide great performance but does not have state
+ */
+export function Visualizer({ number: number }) {
+  return (
+    <div className="visualizer test">
+      {number}
+    </div>
+  );
+}
