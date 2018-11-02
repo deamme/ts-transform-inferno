265cf9c663bd22665c5335bedd5ed2533774b500
deam <deamme@users.noreply.github.com>
2018-11-02 10:05:15 +0100

    Throw on failed tests

--- tests/index.ts
+++ tests/index.ts
@@ -1,9 +1,9 @@
-import * as ts from "typescript";
-import { sync as globSync } from "glob";
-import transform from "../src";
-import { resolve, basename } from "path";
-import { readFileSync, writeFileSync } from "fs";
-import { mkdirpSync } from "fs-extra";
+import * as ts from 'typescript'
+import { sync as globSync } from 'glob'
+import transform from '../src'
+import { resolve, basename } from 'path'
+import { readFileSync, writeFileSync } from 'fs'
+import { mkdirpSync } from 'fs-extra'
 
 const config = {
   experimentalDecorators: true,
@@ -14,51 +14,52 @@ const config = {
   noUnusedLocals: true,
   noUnusedParameters: true,
   stripInternal: true,
-  target: ts.ScriptTarget.ES5
-};
-
-function compile(path: string, callback) {
-  const files = globSync(path);
-  const compilerHost = ts.createCompilerHost(config);
-  const program = ts.createProgram(files, config, compilerHost);
-
-  program.emit(undefined, compare, undefined, undefined, {
-    before: [transform()]
-  });
-
-  callback(files);
+  target: ts.ScriptTarget.ES5,
 }
 
-let failedTests = 0;
-mkdirpSync(resolve(__dirname, "temp/"));
+function compile(path: string, callback) {
+  const files = globSync(path)
+  const compilerHost = ts.createCompilerHost(config)
+  const program = ts.createProgram(files, config, compilerHost)
+
+  program.emit(undefined, compare, undefined, undefined, {
+    before: [transform()],
+  })
+
+  callback(files)
+}
+
+let failedTests = 0
+mkdirpSync(resolve(__dirname, 'temp/'))
 
 function compare(filePath: string, output: string) {
-  const fileBasename = basename(filePath);
-  const referenceFilePath = resolve(__dirname, "references/" + fileBasename);
+  const fileBasename = basename(filePath)
+  const referenceFilePath = resolve(__dirname, 'references/' + fileBasename)
 
-  const tempFilePath = resolve(__dirname, "temp/" + fileBasename);
+  const tempFilePath = resolve(__dirname, 'temp/' + fileBasename)
 
   try {
-    const fileData = readFileSync(referenceFilePath, "utf8");
+    const fileData = readFileSync(referenceFilePath, 'utf8')
     if (fileData !== output) {
-      writeFileSync(tempFilePath, output, "utf8");
-      failedTests++;
+      writeFileSync(tempFilePath, output, 'utf8')
+      failedTests++
     }
   } catch (error) {
-    writeFileSync(tempFilePath, output, "utf8");
-    failedTests++;
+    writeFileSync(tempFilePath, output, 'utf8')
+    failedTests++
   }
 }
 
 function printFinalResult(files: string[]) {
   if (failedTests) {
-    console.log(`${files.length - failedTests}/${files.length} cases passed`);
-    console.log("Please look in the test/temp folder and verify output");
-    console.log("When verified use the command: npm run overwrite-references");
+    console.log(`${files.length - failedTests}/${files.length} cases passed`)
+    console.log('Please look in the test/temp folder and verify output')
+    console.log('When verified use the command: npm run overwrite-references')
+    throw 'Failed tests...'
   } else {
-    console.log(`All cases (${files.length}) successfully passed`);
+    console.log(`All cases (${files.length}) successfully passed`)
   }
 }
-console.time("compile time");
-compile(resolve(__dirname, "cases/*.tsx"), printFinalResult);
-console.timeEnd("compile time");
+console.time('compile time')
+compile(resolve(__dirname, 'cases/*.tsx'), printFinalResult)
+console.timeEnd('compile time')
